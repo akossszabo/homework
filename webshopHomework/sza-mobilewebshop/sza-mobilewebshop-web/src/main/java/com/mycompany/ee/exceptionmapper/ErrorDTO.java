45b14316ca54ff4b687c5d9c95f4310ffd9e09d6
Ákos Szabó <akos.r9@gmail.com>
2016-04-13 13:58:09 +0200

    add project first 04.13

---
+++ webshopHomework/sza-mobilewebshop/sza-mobilewebshop-web/src/main/java/com/mycompany/ee/exceptionmapper/ErrorDTO.java
@@ -0,0 +1,23 @@
+package com.mycompany.ee.exceptionmapper;
+
+
+public class ErrorDTO {
+
+    private String errorMessage;
+
+    public ErrorDTO() {
+        //empty constructor
+    }
+
+    public ErrorDTO(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }   
+}
