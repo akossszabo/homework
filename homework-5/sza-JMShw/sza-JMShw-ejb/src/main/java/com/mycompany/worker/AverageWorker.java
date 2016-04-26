89f2fb18fed2b9e46e3f882d65844cb219e95080
Ákos Szabó <akos.r9@gmail.com>
2016-04-27 01:12:42 +0200

    add JMS hw

---
+++ homework-5/sza-JMShw/sza-JMShw-ejb/src/main/java/com/mycompany/worker/AverageWorker.java
@@ -0,0 +1,43 @@
+package com.mycompany.worker;
+
+import com.mycompany.dto.JobDTO;
+import com.mycompany.dto.MessageDTO;
+import com.mycompany.service.StatisticsBean;
+import java.util.logging.Level;
+import javax.ejb.MessageDriven;
+import javax.inject.Inject;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@MessageDriven(mappedName = "dzsobKju")
+public class AverageWorker implements MessageListener{
+
+    final static Logger LOGGER = LoggerFactory.getLogger(AverageWorker.class);
+    
+    @Inject
+    StatisticsBean statBean;
+    
+    @Override
+    public void onMessage(Message message) {
+        try {
+            JobDTO job = message.getBody(JobDTO.class);
+            Thread.sleep(job.getEstimatedTime()*1000);
+            MessageDTO msg = new MessageDTO();
+            msg.setJobTime(job.getEstimatedTime());
+            if(msg.getJobTime()<5){
+                msg.setDone(true);
+            }else if(msg.getJobTime()>=5){
+                msg.setDone(false);
+            }
+            msg.setNumber(job.getNumber());
+            msg.setTime(System.currentTimeMillis());
+            statBean.finishJobs(msg);
+        } catch (JMSException | InterruptedException ex) {
+            java.util.logging.Logger.getLogger(AverageWorker.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
